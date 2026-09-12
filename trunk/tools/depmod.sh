4cde98a55ac0dd3669573f9419187a19358ba0e4
KuniJiwa <45271734+KuniJiwa@users.noreply.github.com>
2026-09-12 23:06:51 +0800

    fix: correctly package ipset kernel modules into firmware
    - Fix DEPMOD handling so depmod.sh is actually executed
    - Get KERNELRELEASE from kernel Makefile
    - Remove deprecated -r option
    - Only copy necessary ipset modules

--- trunk/tools/depmod.sh
+++ trunk/tools/depmod.sh
@@ -8,6 +8,11 @@ if [ -z "${ROOTDIR}" ]; then
 	exit 1
 fi
 
+# get KERNELRELEASE from kernel Makefile
+if [ -z "${KERNELRELEASE}" ]; then
+  [ -f "${ROOTDIR}/$(LINUXDIR)/Makefile" ] && KERNELRELEASE=$(make -C "${ROOTDIR}/$(LINUXDIR)" -s kernelrelease 2>/dev/null)
+  export KERNELRELEASE
+fi
 if [ -z "${KERNELRELEASE}" ]; then
 	echo "KERNELRELEASE is not set!"
 	exit 1
@@ -23,10 +28,13 @@ if [ ! -x "$depmod_bin" ]; then
 	exit 1
 fi
 
+# These .ko files are only present if CONFIG_FIRMWARE_INCLUDE_IPSET is enabled
 ipset_dir="${ROOTDIR}/build/ipset-7.11/kernel/net/netfilter"
 mkdir -p "${INSTALL_MOD_PATH}/lib/modules/${KERNELRELEASE}/kernel/net/netfilter/ipset"
 cp -f "$ipset_dir/xt_set.ko" "${INSTALL_MOD_PATH}/lib/modules/${KERNELRELEASE}/kernel/net/netfilter"
-cp -f "$ipset_dir/ipset/"*.ko "${INSTALL_MOD_PATH}/lib/modules/${KERNELRELEASE}/kernel/net/netfilter/ipset"
+cp -f "$ipset_dir/ipset/ip_set.ko" "${INSTALL_MOD_PATH}/lib/modules/${KERNELRELEASE}/kernel/net/netfilter/ipset/"
+cp -f "$ipset_dir/ipset/ip_set_hash_ip.ko" "${INSTALL_MOD_PATH}/lib/modules/${KERNELRELEASE}/kernel/net/netfilter/ipset/"
+cp -f "$ipset_dir/ipset/ip_set_hash_net.ko" "${INSTALL_MOD_PATH}/lib/modules/${KERNELRELEASE}/kernel/net/netfilter/ipset/"
 
 if [ "$CONFIG_FIRMWARE_INCLUDE_ANTFS" = "y" ] ; then
 	antfs_dir="${ROOTDIR}/build/antfs-07.19"
@@ -35,7 +43,7 @@ if [ "$CONFIG_FIRMWARE_INCLUDE_ANTFS" = "y" ] ; then
 fi
 
 # call depmod
-$depmod_bin -ae -F System.map -b "${INSTALL_MOD_PATH}" -r ${KERNELRELEASE}
+$depmod_bin -ae -F System.map -b "${INSTALL_MOD_PATH}" ${KERNELRELEASE}
 
 # clear unneeded depmod files
 rm -f "${INSTALL_MOD_PATH}/lib/modules/${KERNELRELEASE}/modules.alias"
